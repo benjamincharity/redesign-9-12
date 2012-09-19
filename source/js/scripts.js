da4a172c18a523edd8d4a38f7542c7be4e119d9d
Benjamin Charity <ben@benjamincharity.com>
2012-09-19 14:37:57 -0600

    added home nav, hid labs for now, fixed page movements and nav focus, added copy

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,61 @@
+// navigation
+$(function() {
+      var $nav = $('#navigation'),
+     $labsPage = $('#labs'),
+  $contactPage = $('#contact'),
+     $homePage = $('#superfluous-titles');
+
+  $nav.on('click', '.i-want-to-go-to-there', function(event) {
+    event.preventDefault();
+
+    if($nav.hasClass('active')) {
+      $nav.removeClass('active');
+    } else {
+      $nav.addClass('active');
+    }
+  });
+
+  $nav.on('click', '.page-nav', function(event) {
+    var $self = $(this);
+
+    event.preventDefault();
+
+    $('.page').removeClass('active goTheOtherWay');
+
+    if($self.attr('href') === '#labs') {
+      console.log('labs');
+      $labsPage.addClass('active');
+    }
+
+    if($self.attr('href') === '#contact') {
+      console.log('contact');
+      $contactPage.addClass('active');
+      $homePage.addClass('goTheOtherWay');
+    }
+
+    if($self.attr('href') === '#home') {
+      console.log('home');
+      $homePage.addClass('active');
+    }
+  });
+});
+
+
+// main page titles
 $(function() {
 
+  $('#superfluous-titles').on('click', 'h1', function(event) {
+    var $self = $(this),
+    $page = $('.page');
+    console.log($self);
+
+    if($self.next().is(':visible')) {
+      $page.removeClass('movinOnUp');
+      $('.hide-yo-kids').slideUp();
+    } else {
+      $page.addClass('movinOnUp');
+      $('.hide-yo-kids').slideUp();
+      $self.next().slideDown();
+    }
+  });
 });
